661134d594f91d38806f7f85eaa3b89b119112f8
renancarvalhops <renancps77@gmail.com>
2025-10-15 15:40:20 -0300

    feat: utils para módulo

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function mod(value: number, modulus: number): number {
+  const valueInt = Math.floor(value);
+  const modulusInt = Math.floor(modulus);
+
+  return ((valueInt % modulusInt) + modulusInt) % modulusInt;
+}
