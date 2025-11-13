48c425ae6f8367a568ad5b70e223bb7aea5440a1
renancarvalhops <renancps77@gmail.com>
2025-11-12 21:19:48 -0300

    feat: coloração de ciclos para o caso R = 2

--- src/lib/graphs/classes/CycleGraph.ts
+++ src/lib/graphs/classes/CycleGraph.ts
@@ -18,19 +18,34 @@ export default class CycleGraph extends GraphClass {
     }
 
     getTotalColoring(order: number): string[][] {
-        const totalChromaticNumber = order % 3 === 0 ? 3 : 4;
+        const R = order % 3;
+        const totalChromaticNumber = R === 0 ? 3 : 4;
         const coloring = Array.from({ length: totalChromaticNumber }, () => Array<string>());
 
-        let i = 0;
-        for (; i < 2 * order - 1; i++) {
-            if (i % 2 === 0) {
-                coloring[i % totalChromaticNumber].push(`${i / 2}`);
-            } else {
-                coloring[i % totalChromaticNumber].push(`${(i - 1) / 2}_${(i + 1) / 2}`);
+        if (R % 2 === 0) {
+            let i = 0;
+            for (; i < 2 * order - 1; i++) {
+                if (i % 2 === 0) {
+                    coloring[i % 3].push(`${i / 2}`);
+                } else {
+                    coloring[i % 3].push(`${(i - 1) / 2}_${(i + 1) / 2}`);
+                }
             }
+
+            coloring[R === 0 ? (i % 3) : 3].push(`0_${(i - 1) / 2}`);            
+        } else {
+            let i = 0;
+            for (; i < 2 * order - 1; i++) {
+                if (i % 2 === 0) {
+                    coloring[i % 4].push(`${i / 2}`);
+                } else {
+                    coloring[i % 4].push(`${(i - 1) / 2}_${(i + 1) / 2}`);
+                }
+            }
+    
+            coloring[i % 4].push(`0_${(i - 1) / 2}`);
         }
 
-        coloring[i % totalChromaticNumber].push(`0_${(i - 1) / 2}`);
 
         return coloring;
     }
