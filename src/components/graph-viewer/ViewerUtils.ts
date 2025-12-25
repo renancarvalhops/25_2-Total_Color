fc20880c74291d8ebe8fbc93a559018e01bfd398
renancarvalhops <renancps77@gmail.com>
2025-12-25 20:13:36 -0300

    style: adição de 50 cores para coloração dos grafos

--- src/components/graph-viewer/ViewerUtils.ts
+++ src/components/graph-viewer/ViewerUtils.ts
@@ -2,14 +2,16 @@ import { mod } from "@/lib/utils";
 
 class HexadecimalColors {
     private static hexColors = [
-        '#3867d6',
-        '#fa8231',
-        '#20bf6b',
-        '#8854d0',
-        '#f7b731',
-        '#a5b1c2',
-        '#4c3a2c',
-        '#000000'   
+        "#E6194B", "#3CB44B", "#FFE119", "#4363D8", "#F58231",
+        "#911EB4", "#46F0F0", "#F032E6", "#BCF60C", "#FABEBE",
+        "#008080", "#E6BEFF", "#9A6324", "#FFD8B1", "#800000",
+        "#AAFFC3", "#808000", "#FF7F00", "#000075", "#FF1493",
+        "#00CED1", "#9400D3", "#32CD32", "#FF4500", "#1E90FF",
+        "#FFD700", "#4B0082", "#00FA9A", "#DC143C", "#00BFFF",
+        "#ADFF2F", "#FF69B4", "#8B4513", "#40E0D0", "#9932CC",
+        "#FF6347", "#7FFF00", "#6495ED", "#D2691E", "#20B2AA",
+        "#FF8C00", "#6A5ACD", "#9ACD32", "#BA55D3", "#00FF7F",
+        "#CD5C5C", "#4682B4", "#B8860B", "#00CED1", "#000000"
     ];
 
     public static get(color: number) {
